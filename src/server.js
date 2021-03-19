98b0c6cdb54767b1b786c1943352fc0717b179a3
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-03-19 14:02:15 -0300

    primeiro hello world com handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const path = require('path');
 
 // Initializations
@@ -7,15 +8,22 @@ const app = express();
 //Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.engine('.hbs', exphbs({
+     defaultLayout: 'main',
+     layoutsDir: path.join(app.get('views'), 'layouts'),
+     partialsDir: path.join(app.get('views'), 'patials'),
+     extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
 
 //Global Variables
 
 //routes
-app.get('/',(req,res)=>{
-     return res.send('ola brasil')
+app.get('/', (req, res) => {
+     return res.render('index')
 })
 
 //static files

3d74b8a9b3dd7c1e3a57a451c65ce5d1c3254018
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-03-20 22:16:09 -0300

    add mongodb

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-const {NOTE_APP_HOST, NOTE_APP_DATABASE} = process.env
-const MONGODB_URL = `mongodb://${NOTE_APP_HOST}/${NOTE_APP_DATABASE}` || 'mongodb://localhost/dbName';
-
+const { NOTE_APP_HOST, NOTE_APP_DATABASE, NOTE_APP_PASSWORD } = process.env
+const MONGODB_URL = `mongodb+srv://${NOTE_APP_HOST}:${NOTE_APP_PASSWORD}@cluster0.vedqo.mongodb.net/${NOTE_APP_DATABASE}?retryWrites=true&w=majority` || 'mongodb://localhost/dbName';
 mongoose.connect(MONGODB_URL, {
      useUnifiedTopology: true,
-     useNewUrlParser: true
-}).then(db=>console.log('Database está conectado'))
-.catch(err=>console.log('Erro DB -> ', err));
+     useNewUrlParser: true,
+}).then(db => console.log('Database está conectado'))
+     .catch(err => console.log('Erro DB -> ', err));
+
+// mongodb+srv://Note_App:adm123321Adm@cluster0.vedqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority

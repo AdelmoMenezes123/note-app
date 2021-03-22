00a26eb73f61875c869c58b1cc3c9d95e788d906
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-03-22 14:06:58 -0300

    finaliza front-end

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,23 +1,26 @@
 //Animação menu responsivo
 $('.btn-menu, .btn-close').on('click', function () {
-    $('.menu').slideToggle('slow');
-  });
-  
-  $('nav ul li').click(function () {
-    $('.menu').hide();
-  });
-  
-  //Scroll menu
-  $('nav a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top;
-  
-    $('html, body').animate(
-        {
-            scrollTop: targetOffset,
-        },
-        1000
-    );
-  });
-  
+  $('.menu').slideToggle('slow');
+});
+
+$('nav ul li').click(function () {
+  $('.menu').hide();
+});
+
+//Scroll menu
+$('nav a[href^="#"]').on('click', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: targetOffset,
+    },
+    1000
+  );
+});
+
+setTimeout(function() {
+  $("#msg").hide();
+}, 3000)

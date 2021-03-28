c165b46b3d16d314f88abd676141205a18d91277
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-03-28 03:24:57 -0300

    add regra, cada usuarios tem suas notas

--- src/controller/note.controller.js
+++ src/controller/note.controller.js
@@ -12,19 +12,20 @@ noteCrlt.createNewNote = async (req, res) => {
 
     req.flash('success_msg', 'Note Added Successfully')
 
+    newNote.user = req.user.id;
     await newNote.save().then(success => {
         res.redirect('/notes/all-notes')
     }).catch(err => console.log(err))
 }
 
 noteCrlt.renderNote = async (req, res) => {
-
-    const hqs = await Note.find({ "categoria": "hqs" })
-    const mangas = await Note.find({ "categoria": "mangas" })
-    const livros = await Note.find({ "categoria": "livros" })
-    const filmes = await Note.find({ "categoria": "filmes" })
-    const series = await Note.find({ "categoria": "series" })
-    const animes = await Note.find({ "categoria": "animes" })
+    
+    const hqs = await Note.find({ "categoria": "hqs", user: req.user.id })
+    const mangas = await Note.find({ "categoria": "mangas", user: req.user.id })
+    const livros = await Note.find({ "categoria": "livros", user: req.user.id })
+    const filmes = await Note.find({ "categoria": "filmes", user: req.user.id })
+    const series = await Note.find({ "categoria": "series", user: req.user.id })
+    const animes = await Note.find({ "categoria": "animes", user: req.user.id })
     const animacoes = await Note.find({ "categoria": "animacoes" })
 
     res.render('notes/all-notes', { hqs, mangas, livros, filmes, series, animes, animacoes });
@@ -32,11 +33,17 @@ noteCrlt.renderNote = async (req, res) => {
 
 noteCrlt.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id)
+
+    if(note.user != req.user.id){
+        return res.redirect('/notes/all-notes')
+    }
+
     res.render('notes/edite-note', { note })
 }
 
 noteCrlt.updateNote = async (req, res) => {
     const { title, description } = req.body;
+
     await Note.findByIdAndUpdate(req.params.id, { title, description })
 
     req.flash('success_msg', 'Note updated Successfully')
--- src/models/Note.js
+++ src/models/Note.js
@@ -3,9 +3,10 @@ const { Schema, model } = require('mongoose');
 const NotSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    categoria: { type: String, required: true},
+    categoria: { type: String, required: true },
+    user: { type: String, required: true }
 },
-{ timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = model('Note', NotSchema);
